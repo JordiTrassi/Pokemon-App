bf8ec43c302e06f1a4d9b5c8a9c2f71bdad67c1f
JordiTrassi <j.trassi@telefonica.net>
2022-09-14 15:58:03 +0200

    Added ErrorPage styles

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,10 +1,25 @@
+import { Grid } from '@mui/material';
+
 
 export const PokemonApp = () => {
   
   return (
-    <>
-      <h1>POKEMON APP</h1>
-    </>
+    
+    <Grid
+      container
+      sx={{
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: 'url("https://images6.alphacoders.com/305/305896.jpg")',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          xs: ml=20, 
+        }}
+      >
+
+    </Grid>
+
   )
 };
 
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,32 @@
 import { useRouteError } from 'react-router-dom';
+import { Grid, Typography } from '@mui/material';
 
 export const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Grid
+      container
+      id="error-page"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Grid
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        spacing={5}
+      >
+        <Typography variant='h2' padding={5}>Oops!</Typography>
+        <Typography variant='h4' padding={2}>Sorry, an unexpected error has occurred.</Typography>
+        <Typography variant='h5'><i>{error.statusText || error.message}</i></Typography>
+      </Grid>
+    </Grid>
   );
 }
