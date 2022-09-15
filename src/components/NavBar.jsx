c1b3e3357ac622f07d21caaf6cadb3f6d54f4881
JordiTrassi <j.trassi@telefonica.net>
2022-09-15 15:32:17 +0200

    Installed React-Redux and created NavBar structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,140 @@
+// import { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+
+import { AppBar, Box, Toolbar, Typography, InputBase, Link, Tooltip, FormControl } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+
+// import { verifyInputValue } from '../helpers/verifyInputValue';
+// import Swal from 'sweetalert2';
+// import 'sweetalert2/dist/sweetalert2.css';
+
+// import { getAlbums, startLoadingAlbums } from '../store';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '20ch',
+      '&:focus': {
+        width: '30ch',
+      },
+    },
+  },
+}));
+
 
 export const NavBar = () => {
+
+  // const dispatch = useDispatch();
+  // const [inputValue, setInputValue] = useState('');
+  // const { errorMessage } = useSelector(state => state.playList);
+
+  // const onInputChange = ({target}) => {
+  //   setInputValue(target.value);
+  // }
+
+  // const onSubmit = async () => {
+  //   const verifiedInputValue = await verifyInputValue(inputValue);
+
+  //   dispatch(startLoadingAlbums({ verifiedInputValue }));
+  //   dispatch(getAlbums({ verifiedInputValue }));
+  // }
+
+  // useEffect(() => {
+  //   const listener = event => {
+  //     if (event.code === "Enter" || event.code === "NumpadEnter") {
+  //       event.preventDefault();
+  //       onSubmit();
+  //     }
+  //   };
+  //   document.addEventListener("keydown", listener);
+  //   return () => {
+  //     document.removeEventListener("keydown", listener);
+  //   };
+  // }, [inputValue]);
+
+  // useEffect(() => {
+  //     if (errorMessage.length > 0) {
+  //         Swal.fire({
+  //             icon: 'error',
+  //             title: 'Oops...',
+  //             text: errorMessage,
+  //             })
+  //     }
+  // }, [errorMessage])
+
   return (
-    <div>
-      
-    </div>
-  )
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>          
+          <Typography
+                variant="h5"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: '2', sm: 'block' } }}
+          >
+            <Tooltip
+              title="home"
+              arrow
+              placement="bottom"  
+            >
+              <Link
+                color="inherit"
+                underline="none" 
+                href="/"
+                >
+                  {/* <MusicNote sx={{fontSize: 35}} /> MUSIC APP */}
+              </Link>
+            </Tooltip>
+          </Typography>
+          <Search>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+            <FormControl>
+              {/* onSubmit={onSubmit} */}
+                  <StyledInputBase
+                    type="text"
+                    placeholder="Search Pokemon"
+                    value={inputValue}
+                    onChange={onInputChange}
+                  />
+                </FormControl>
+            </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-
--- src/pages/PokemonsListPage.jsx
+++ src/pages/PokemonsListPage.jsx
@@ -1,9 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
+import { NavBar } from '../components';
 
 
 export const PokemonsList = () => {
   return (
     <>
+      <NavBar />
       <h1>pokemons list!</h1>
       <h2><Link to={`:pokemonId`}>DETAILS</Link></h2>
       <Outlet />
