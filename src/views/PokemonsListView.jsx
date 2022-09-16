af8633d321c95271443b6ee6d5070981678c7ef1
JordiTrassi <j.trassi@telefonica.net>
2022-09-16 12:03:04 +0200

    Crated PokemonListView structure

--- src/views/PokemonsListView.jsx
+++ src/views/PokemonsListView.jsx
@@ -1,10 +1,28 @@
+import { useSelector } from 'react-redux';
+import { List} from '@mui/material';
+import { PokemonCard, IsLoading } from '../components';
+
 
 export const PokemonsListView = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    
+    const { isLoading, pokemons = [] } = useSelector(state => state.pokemonStore);
+
+
+    return (
+        
+        <List sx={{ width: '100%', p: 1 }}>
+            
+            {
+              (isLoading)
+                  ? <IsLoading />
+                  : pokemons.map(pokemon => (
+                      <PokemonCard                    
+                          key={pokemon.name}
+                          {...pokemon}
+                      />
+                    ))
+            }
+            
+        </List>
+    );
 }
-
-
